c71764539eabbf4e990ac668ad8deffeb8470ece
Francis Fish <francisfish@fs.co.uk>
2025-05-23 15:24:50 +0100

    add Glog command to fugitive

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,1 +1,7 @@
-return { "tpope/vim-fugitive", lazy = false }
+return {
+  "tpope/vim-fugitive",
+  lazy = false,
+  config = function()
+    vim.api.nvim_create_user_command('Glog', ':Git log --pretty=format:"%h %ad %an %s" --date=format:"%Y/%m/%d %H:%M"', {})
+  end
+}

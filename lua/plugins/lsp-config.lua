df084b0e6afdc13932afc1ff3a0a6c71834e98da
Francis Fish <f@francis.fish>
2026-03-22 19:53:20 +0000

    Fix stuffs

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -14,14 +14,7 @@ return {
     },
     config = function()
       require("mason-lspconfig").setup {
-        automatic_enable = {
-          "lua_ls",
-          "vimls",
-          "solargraph",
-          "html",
-          "elixirls",
-          "ruby_lsp",
-        }
+        automatic_enable = false,
       }
     end,
   },
@@ -29,27 +22,23 @@ return {
     "neovim/nvim-lspconfig",
     lazy = false,
     config = function()
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local servers = {
+        "ts_ls",
+        "solargraph",
+        "html",
+        "lua_ls",
+        "elixirls",
+        "ruby_lsp",
+      }
 
-      local lspconfig = require("lspconfig")
-      lspconfig.ts_ls.setup({
-        capabilities = capabilities
-      })
-      lspconfig.solargraph.setup({
-        capabilities = capabilities
-      })
-      lspconfig.html.setup({
-        capabilities = capabilities
-      })
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities
-      })
-      lspconfig.elixirls.setup({
-        capabilities = capabilities
-      })
-      lspconfig.ruby_lsp.setup({
-        capabilities = capabilities
-      })
+      for _, server in ipairs(servers) do
+        vim.lsp.config(server, {
+          capabilities = capabilities,
+        })
+        vim.lsp.enable(server)
+      end
+
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
